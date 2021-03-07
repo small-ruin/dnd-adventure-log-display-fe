8611c59289a3e80d2bbf7f5abacc4d84950eb8ea
hilshire <hillshire.triela@gmail.com>
2021-03-07 20:43:36 +0800

    buttom button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import './Button.css';
 
 interface ButtonProps {
-    onClick: () => void;
+    onClick?: () => void;
+    style?: React.CSSProperties;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick}) => {
-    return <div className="button" onClick={onClick}>{children}</div>
+const Button: React.FC<ButtonProps> = ({ children, onClick, style}) => {
+    return <div className="button" onClick={onClick} style={style}>{children}</div>
 }
 
 export default Button;
--- src/pages/Log/index.tsx
+++ src/pages/Log/index.tsx
@@ -4,17 +4,39 @@ import { get } from '../../utils';
 import { useParams } from 'react-router-dom';
 import './Log.css';
 import Loading from '../../components/Loading'
+import Button from '../../components/Button';
 
 interface Params {
   id: string
 }
+
+let lastScrollTop = 0
+
 export default function LogComp() {
   const { id } = useParams<Params>();
 
-  const [log, setLog] = useState<Log | null>(null)
+  const [log, setLog] = useState<Log | null>(null);
+  const [showSetting, setShowSetting] = useState<boolean>(true);
+
 
   useEffect(() => {
     get('/log/' + id).then(res => setLog(res.data))
+
+    document.addEventListener("scroll", e => handleScroll());
+
+    function handleScroll() {
+        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        console.log(st, lastScrollTop)
+        if (st > lastScrollTop){
+            console.log('down')
+            setShowSetting(false)
+        } else {
+            console.log('up')
+            setShowSetting(true)
+        }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -22,11 +44,19 @@ export default function LogComp() {
     return <Loading></Loading>
   }
 
+
+
   return (
     <div className="log mainContent">
       <h1>{ log.name }</h1>
       <h3 className="grey-title">{ log.createdAt }</h3>
       <div dangerouslySetInnerHTML={{ __html: log.content}}></div>
+      <Button style={{ position: 'fixed', bottom: '5%', right: '5%',
+        borderRadius: '50%', width: '40px', height: '40px', padding: 0, textAlign: 'center', lineHeight: '40px',
+        display: showSetting ? 'block' : 'none' }}>设置</Button>
     </div>
   )
+
+
+
 }
