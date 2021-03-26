f2d0b2a1475e2c66800571765f0b157d1398ced6
hilshire <hillshire.triela@gmail.com>
2021-03-26 16:44:07 +0800

    url and proxy

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotFound() {
+    return <div>你来到了未知的位面</div>
+}
--- src/request/request.ts
+++ src/request/request.ts
@@ -1,16 +1,28 @@
 import axios from 'axios';
-import { baseUrl } from './url';
+import { baseUrl, Urls } from './url';
 
-axios.defaults.baseURL = baseUrl + '/api';
+
+axios.defaults.baseURL = baseUrl + 'api';
 
 axios.interceptors.response.use(
     function(res) {
         return res
     },
     function(error) {
-        if (error.response?.status === 400) {
-            alert(error.response?.data?.message);
-        }
+        const statusCode = error.response?.data.statusCode;
+        setTimeout(() => {
+            switch (statusCode) {
+                case 404:
+                    window.location.href = Urls.NOT_FOUND;
+                    break;
+                case 400:
+                    alert(error.response?.data?.message);
+                    break;
+                default:
+                    window.location.href = Urls.ERROR;
+            }
+        })
+
         return Promise.reject(error);
     })
 
